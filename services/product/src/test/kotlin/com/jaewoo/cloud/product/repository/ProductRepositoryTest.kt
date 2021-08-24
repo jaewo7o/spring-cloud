48295c372ea328abc6d7064c00110eb8db2cc67d
Jung Jaewoo <jaewo7o@naver.com>
2021-08-24 13:29:11 +0900

    product 단위테스트 완료

--- services/product/src/test/kotlin/com/jaewoo/cloud/product/repository/ProductRepositoryTest.kt
+++ services/product/src/test/kotlin/com/jaewoo/cloud/product/repository/ProductRepositoryTest.kt
@@ -2,6 +2,7 @@ package com.jaewoo.cloud.product.repository
 
 import com.jaewoo.cloud.product.builder.ProductBuilder
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
@@ -11,30 +12,37 @@ class ProductRepositoryTest {
     @Autowired
     lateinit var productRepository: ProductRepository
 
+    @BeforeEach
+    fun setup() {
+        // 전체삭제
+        productRepository.deleteAll()
+    }
+
     @Test
     fun `Product 단건 저장-조회 테스트`() {
-        println("test ====>>>>")
         // given
         val buildProduct = ProductBuilder().buildProduct()
-        productRepository.save(buildProduct)
+        val saveProduct = productRepository.save(buildProduct)
 
         // when
-        val findByProduct = productRepository.findByProductId(buildProduct.productId)!!
+        val findByProduct = productRepository.findByProductId(saveProduct.productId)!!
 
         // then
-        Assertions.assertThat(findByProduct.productId).isEqualTo(buildProduct.productId)
-        Assertions.assertThat(findByProduct.productName).isEqualTo(buildProduct.productName)
-        Assertions.assertThat(findByProduct.productInfo).isEqualTo(buildProduct.productInfo)
+        Assertions.assertThat(findByProduct.productId).isEqualTo(saveProduct.productId)
+        Assertions.assertThat(findByProduct.productName).isEqualTo(saveProduct.productName)
+        Assertions.assertThat(findByProduct.productInfo).isEqualTo(saveProduct.productInfo)
     }
 
     @Test
     fun `Product 업데이트 테스트`() {
         // given
+        val updateProductName = "UPDATE PRODUCTNAME"
+
         val buildProduct = ProductBuilder().buildProduct()
         var saveProduct = productRepository.save(buildProduct)
 
         // when
-        saveProduct.productName = buildProduct.productName + " UPDATE"
+        saveProduct.productName = updateProductName
         productRepository.save(saveProduct)
 
         // when
@@ -42,7 +50,7 @@ class ProductRepositoryTest {
 
         // then
         Assertions.assertThat(findByProduct.productId).isEqualTo(buildProduct.productId)
-        Assertions.assertThat(findByProduct.productName).isEqualTo(buildProduct.productName + " UPDATE")
+        Assertions.assertThat(findByProduct.productName).isEqualTo(updateProductName)
         Assertions.assertThat(findByProduct.productInfo).isEqualTo(buildProduct.productInfo)
     }
 
